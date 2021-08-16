4fb5874f1f9a34a8ba66d8898e1fe669742f407d
sunwei <sw122802@163.com>
2021-08-16 17:06:00 +0800

    FirstCommitV1

---
+++ src/main/java/com/atguigu/crud/test/MBG.java
@@ -0,0 +1,31 @@
+package com.atguigu.crud.test;
+/* *
+ *@Description:
+ * @Return:
+ * @Author: 孙维
+ * @单位：湖南农业大学物联网工程一班
+ * @Create:2021-08-15 20:12
+ * @开发版本：综合练习v0.1
+ */
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MBG {
+    public static void main(String[] args) throws Exception{
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sunwei
+  Date: 2021/8/15
+  Time: 12:08
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <link rel="stylesheet" href="static/bootstrap-3.4.1-dist/css/bootstrap.min.css">
+    <script src="static/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
+    <script src="static/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+<button type="button" class="btn btn-success">（成功）Success</button>
+</body>
+</html>
